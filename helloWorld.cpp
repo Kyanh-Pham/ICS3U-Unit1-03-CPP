d04e35e92cf94bb98153c869e84c32e69be21388
Kyanh Pham <kyanh.pham@mths.ca>
2022-09-16 14:45:12 +0000

    added Done.

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Kyanh Pham
+//
+// Created by: Kyanh Pham
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    std::cout << "\nDone.\n";
+}
